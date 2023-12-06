5b048e613beaca00d8fde3eef8c39f65b80ce3c7
김성준 <q881229@gmail.com>
2023-12-06 11:31:40 +0900

    nextjs mongodb 연결후 데이터 뿌리기

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,18 @@ export default async function Home() {
 
   const client = await connectDB;
   const db = client.db("forum")
-  let result :string[] = await db.collection('post').find().toArray()
+  // mongodb안에 있는 데이터들을 가져와서 모두 array로 변환
+  let result: string[] = await db.collection('post').find().toArray()
   console.log(result)
+  console.log(result[0].title)
+  console.log(result[0].content)
 
   return (
     <div>
-      <h4>하이</h4>
+      <h4>게시판입니다.</h4>
+
+      <h5>제목 : {result[0].title}</h5>
+      <h5>내용 : {result[0].content}</h5>
     </div>
   )
 }
