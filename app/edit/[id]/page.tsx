b0b0d599fb9e82debf957058dcda80de054002f1
김성준 <q881229@gmail.com>
2023-12-16 00:00:09 +0900

    복습

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -17,6 +17,19 @@ export default function Edit() {
       <input name="content" placeholder="글내용" />
       <button type="submit">버튼</button>
     </form>
+    <form action="/api/post/new" method="POST">
+      <input name="title" placeholder="글제목" />
+      <input name="content" placeholder="글내용" />
+      <button type="submit">버튼</button>
+    </form>
+    
+    <form action="/api/post/new" method="POST">
+      <input name="title" placeholder="글제목" />
+      <input name="content" placeholder="글내용" />
+      <button type="submit">버튼</button>
+    </form>
+    
+    
     {/* <h4>글작성</h4>
     <form action="/api/date" method="GET">
       <button type="submit">버튼</button>
