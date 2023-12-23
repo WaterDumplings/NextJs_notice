67649800f42e52751570234e7d549c03951dadcb
김성준 <q881229@gmail.com>
2023-12-23 23:58:56 +0900

    글수정페이지

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,4 +1,12 @@
-export default function Edit() {
+import { connectDB } from "@/util/database"
+import { ObjectId } from "mongodb";
+
+export default async function Edit() {
+
+  const client = await connectDB;
+  const db = client.db("forum")
+  let result: string[] = await db.collection('post').findOne({_id : new ObjectId(props.params.id)})
+
   return (
     <div className="p-20">
     <h4>수정페이지</h4>
@@ -7,28 +15,6 @@ export default function Edit() {
       <input name="content" placeholder="글내용" />
       <button type="submit">버튼</button>
     </form>
-    <form action="/api/post/new" method="POST">
-      <input name="title" placeholder="글제목" />
-      <input name="content" placeholder="글내용" />
-      <button type="submit">버튼</button>
-    </form>
-    <form action="/api/post/new" method="POST">
-      <input name="title" placeholder="글제목" />
-      <input name="content" placeholder="글내용" />
-      <button type="submit">버튼</button>
-    </form>
-    <form action="/api/post/new" method="POST">
-      <input name="title" placeholder="글제목" />
-      <input name="content" placeholder="글내용" />
-      <button type="submit">버튼</button>
-    </form>
-    
-    <form action="/api/post/new" method="POST">
-      <input name="title" placeholder="글제목" />
-      <input name="content" placeholder="글내용" />
-      <button type="submit">버튼</button>
-    </form>
-    
     
     {/* <h4>글작성</h4>
     <form action="/api/date" method="GET">
